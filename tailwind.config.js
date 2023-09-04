bfbfef6defa9145784f2d85e02a29d181edf5e92
PandaZenCode <okrafaldino37@gmail.com>
2023-09-04 20:11:10 +0700

    hero section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#7D5A50",
+        secondary: "#798777",
+        light: "#A2B29F",
+        dark: "#594545",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
